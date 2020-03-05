cb072a77c05a0b895896fe6c2ab8478d7d6eb459
openxw <openxw@qq.com>
2020-03-05 15:12:34 +0800

    基础页面结构

---
+++ resources/views/layouts/_footer.blade.php
@@ -0,0 +1,22 @@
+{{-- <footer class="footer">
+  <div class="container">
+    <p class="float-left">
+      由<a href="http://cditd.com">Alan</a> 设计和编码 <span style="color: #e27575;font-size: 14px;">💛</span>
+    </p>
+
+    <p class="float-right">
+      <a href="mailto:openxw@gmail.com">联系我们</a>
+    </p>
+  </div>
+
+</footer> --}}
+
+<footer class="footer">
+  <div class="container">
+    <p class="float-left">
+      由 <a href="http://weibo.com/u/1837553744?is_hot=1" target="_blank">Alan</a> 设计和编码 <span style="color: #e27575;font-size: 14px;">❤</span>
+    </p>
+
+    <p class="float-right"><a href="mailto:openxw@gmail.com">联系我们</a></p>
+  </div>
+</footer>
